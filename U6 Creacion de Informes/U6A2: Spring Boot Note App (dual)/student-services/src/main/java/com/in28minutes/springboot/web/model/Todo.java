f07c48a565f03104026aa74f3d8d1699a999a937
Ismael Bernad Tello <bernad.teism20@zaragoza.salesianos.edu>
2023-11-24 16:19:15 +0100

    U6A2 Terminado

---
+++ U6 Creacion de Informes/U6A2: Spring Boot Note App (dual)/student-services/src/main/java/com/in28minutes/springboot/web/model/Todo.java
@@ -0,0 +1,20 @@
+package com.in28minutes.springboot.web.model;
+
+import java.util.Date;
+
+public class Todo {
+    public Todo(int i, String string, String string2, Date date, boolean b) {
+
+    }
+    private int id;
+    private String user;
+    private String desc;
+    private Date targetDate;
+    private boolean isDone;
+    
+    public Object getUser() {
+        return null;
+    }
+
+//Getters, Setters, Constructors, toString, equals and hash code
+} 
