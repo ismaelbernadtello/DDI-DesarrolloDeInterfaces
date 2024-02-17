04589c6c8044531b801a154918fb6f9bd1d7f174
Ismael Bernad Tello <bernad.teism20@zaragoza.salesianos.edu>
2024-02-17 10:12:53 +0100

    U7A1 Terminado

--- U7 Documentacion de aplicaciones/U7A1 Documentación Rest API/openapidemo/apiserver/src/main/java/com/xyz/openapi/server/controller/OrganizationController.java
+++ U7 Documentacion de aplicaciones/U7A1 Documentación Rest API/openapidemo/apiserver/src/main/java/com/xyz/openapi/server/controller/OrganizationController.java
@@ -26,14 +26,14 @@ public class OrganizationController {
     private List<Employee> employees = new ArrayList<>();
     private List<Department> departments = new ArrayList<>();
 
-    @GetMapping(value = "/employees")
-    @Operation(summary = "If departmentId is not passed, get all employees. Otherwise get employees from specific department.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", content = {
-                    @Content(schema = @Schema(implementation = EmployeeList.class))
+    @GetMapping(value = "/employees") // Metodo para obtener todos los empleados
+    @Operation(summary = "If departmentId is not passed, get all employees. Otherwise get employees from specific department.") // Descripcion del metodo
+    @ApiResponses({ // Respuestas posibles
+            @ApiResponse(responseCode = "200", content = { // Respuesta exitosa
+                    @Content(schema = @Schema(implementation = EmployeeList.class)) //Se devuelve un objeto EmployeeList
             }),
-            @ApiResponse(responseCode = "500", content = {
-                    @Content(schema = @Schema(implementation = ErrorResponse.class))
+            @ApiResponse(responseCode = "500", content = { // Respuesta de error
+                    @Content(schema = @Schema(implementation = ErrorResponse.class)) //Se devuelve un objeto ErrorResponse
             })
     })
     public EmployeeList getAllEmployees(@RequestParam(required = false) String deptId) {
@@ -44,7 +44,7 @@ public class OrganizationController {
         return EmployeeList.of(employees);
     }
 
-    @GetMapping(value = "/employees/{id}")
+    @GetMapping(value = "/employees/{id}") // Metodo para obtener un empleado por id
     public Employee getEmployee(@PathVariable String id) {
         Optional<Employee> optional = employees.stream()
                 .filter(emp -> emp.getId().equals(id))
@@ -55,14 +55,14 @@ public class OrganizationController {
         return optional.get();
     }
 
-    @PostMapping(value = "/employees")
+    @PostMapping(value = "/employees") // Metodo para crear un empleado
     public String createEmployee(@RequestBody Employee emp){
         emp.setId(UUID.randomUUID().toString());
         employees.add(emp);
         return emp.getId();
     }
 
-    @PutMapping(value = "/employees")
+    @PutMapping(value = "/employees") // Metodo para actualizar un empleado
     public String updateEmployee(Employee updatedEmp){
         employees.stream()
                 .filter(e -> updatedEmp.getId().equals(e.getId()))
@@ -75,12 +75,12 @@ public class OrganizationController {
         return updatedEmp.getId();
     }
 
-    @GetMapping(value = "/departments")
+    @GetMapping(value = "/departments") // Metodo para obtener todos los departamentos
     public List<Department> getAllDepartments() {
         return departments;
     }
 
-    @GetMapping(value = "/departments/{id}")
+    @GetMapping(value = "/departments/{id}") // Metodo para obtener un departamento por id
     public Department getDepartment(@PathVariable String id) {
         Optional<Department> optional = departments.stream()
                 .filter(emp -> emp.getId().equals(id))
@@ -91,7 +91,7 @@ public class OrganizationController {
         return optional.get();
     }
 
-    @PostMapping(value = "/departments")
+    @PostMapping(value = "/departments") // Metodo para crear un departamento
     public String createDepartment(Department department){
         department.setId(UUID.randomUUID().toString());
         departments.add(department);
